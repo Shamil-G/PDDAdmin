9fd2e5079feaa5c8e8e713ab1465ad59fcc10251
Shamil-G <s.gusseynov@gmail.com>
2021-12-12 20:19:46 +0600

    Добавлен статус рабочей станции T - for testing O - for operator

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 set echo on
 #python -m install virtualenv
-python -m venv venv
-C:\Shamil\PDDAdmin\venv\Scripts\activate.bat
-C:\Shamil\PDDAdmin\venv\Scripts\python.exe -m pip install --upgrade pip
-C:\Shamil\PDDAdmin\venv\Scripts\pip install flask
+#python -m venv venv
+#e:\Shamil\PDDAdmin\venv\Scripts\activate.bat
+e:\Shamil\PDDAdmin\venv\Scripts\python.exe -m pip install --upgrade pip
+e:\Shamil\PDDAdmin\venv\Scripts\pip install flask
 pip install flask_login
 pip install flask_sqlalchemy
 pip install cx_Oracle
